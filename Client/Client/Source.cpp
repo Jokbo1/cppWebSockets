8864a2223e3a0ce3386dd427b93e43f384d0de4a
Sauzande,Gásten G.S.K <g.sauzande@student.fontys.nl>
2016-12-16 16:50:44 +0100

    Clients send dynamic messages. Both clients and servers know when someone disconnects

--- Client/Client/Source.cpp
+++ Client/Client/Source.cpp
@@ -8,19 +8,143 @@
 #include <string>
 SOCKET connection;
 
+std::string serverIP = "145.85.48.4";
+int serverPort = 1337;
+
+enum Packet {
+	//All the different packet types that can be sent
+	P_ChatMessage,
+	P_Test
+};
+
+bool sendInt(int num) {
+	int retCheck = send(connection, (char*)&num, sizeof(int), NULL); //send the int
+
+	if (retCheck == SOCKET_ERROR) {
+		//connection problem
+		return false;
+	}
+
+	return true;
+
+}
+
+bool getInt(int &num) {
+	int retCheck = recv(connection, (char*)&num, sizeof(int), NULL); //receive buffer lenght
+	 //send the int
+
+	if (retCheck == SOCKET_ERROR) {
+		//connection problem
+		return false;
+	}
+
+	return true;//Sucessfully received int
+
+}
+
+bool sendPacketType(Packet packettype) {
+	int retCheck = send(connection, (char*)&packettype, sizeof(Packet), NULL); //Sending the packet type
+
+	if (retCheck == SOCKET_ERROR) {
+		//connection problem
+		return false;
+	}
+
+	return true;
+
+}
+
+bool getPacketType(Packet &packettype) {
+	int retCheck = recv(connection, (char*)&packettype, sizeof(Packet), NULL); 
+
+	if (retCheck == SOCKET_ERROR) {
+		//connection problem
+		return false;
+	}
+
+	return true;//Sucessfully received int
+
+}
+
+bool sendString(std::string &text) {
+	if (!sendPacketType(P_ChatMessage)) {//sending the packat type
+		return false;
+	}
+
+	int bufferlength = text.size();
+	if (!sendInt(bufferlength)) {//sending the length of the message
+		return false;
+	}
+
+	int retCheck = send(connection, text.c_str(), bufferlength, NULL); //Sending the packet type
+
+	if (retCheck == SOCKET_ERROR) {
+		//connection problem
+		return false;
+	}
+	
+	return true;
+
+}
+
+bool getString(std::string &text) {
+	int bufferLength;
+
+	if (!getInt(bufferLength)) {
+		return false;
+	}
+
+	char * buffer = new char[bufferLength + 1];//Allocating memory to a dynamic sized char array.
+	buffer[bufferLength] = '\0';//Adding the null terminator to the end of the string
+	
+	int retCheck = recv(connection, buffer, bufferLength, NULL);
+	text = buffer;//set the string to received buffer message
+	delete[] buffer; //everytime we use a "new" we should put a "delete" after we use it
+
+	if (retCheck == SOCKET_ERROR) {
+		//connection problem
+		return false;
+	}
+
+	return true;//Sucessfully received int
+
+}
+
+bool processPacket(Packet packettype) {
+	switch (packettype) {
+	case P_ChatMessage:
+	{
+		std::string message;
+		if (!getString(message)) {
+			return false;
+		}
+
+		std::cout << message << std::endl;
+
+		break;
+	}
+
+	default:
+		std::cout << "Unrecognized packet:" << packettype << std::endl;
+		break;
+	}
+	return true;
+}
 //client thread to receive message
 void clientThread() {
-	int bufferLength;
+	Packet packettype;
 	while (true) {
-		recv(connection, (char*)&bufferLength, sizeof(int), NULL); //receive buffer lenght
-		char * buffer = new char[bufferLength + 1];//Allocating memory to a dynamic sized char array.
-		buffer[bufferLength] = '\0';//Adding the null terminator to the end of the string
-		recv(connection, buffer, bufferLength, NULL);
-		std::cout << "Message:" << buffer << std::endl;
-		
-		delete[] buffer; //everytime we use a "new" we should put a "delete" after we use it
-		
+
+		if (!getPacketType(packettype)) {
+			break;
+		}
+		if (!processPacket(packettype)) {
+			//if we couldn't process the packet then get out of the loop
+			break;
+		}
 	}
+	std::cout << "Lost connection to the server." << std::endl;
+	closesocket(connection); //Closing the socket once we are done with the connection
 }
 
 int main() {
@@ -36,8 +160,8 @@ int main() {
 
 	SOCKADDR_IN addr; //Address that we will bind our listeing socket to
 	int addrlen = sizeof(addr); //length of the address
-	addr.sin_addr.s_addr = inet_addr("127.0.0.1");//which address will the server be hosted on
-	addr.sin_port = htons(1111);//port. WHY NO FUNCTION FOR BOTH?
+	addr.sin_addr.s_addr = inet_addr(serverIP.c_str());//which address will the server be hosted on
+	addr.sin_port = htons(serverPort);//port. WHY NO FUNCTION FOR BOTH?
 	addr.sin_family = AF_INET;//Specifies an IPv4 type of socket
 
 	connection = socket(AF_INET, SOCK_STREAM, NULL);
@@ -51,12 +175,12 @@ int main() {
 	//recv(connection, MOTD, sizeof(MOTD), NULL); // Receive message from the server, passing the size , the message
 	//std::cout << "MOTD:" << MOTD << std::endl;
 
-	std::string buffer;
+	std::string input;
 	while (true) {
-		std::getline(std::cin,buffer);//reading input
-		int bufferlength = buffer.size();
-		send(connection, (char*)&bufferlength, sizeof(int), NULL); //Send integer that holds the size of the message in bytes
-		send(connection, buffer.c_str(), bufferlength, NULL); //c_str() converts the string into and array of chars.
+		std::getline(std::cin,input);//reading input
+		if (!sendString(input)) {
+			return false;
+		}
 		Sleep(10);
 	}
 
--- Server/Server/Source.cpp
+++ Server/Server/Source.cpp
@@ -8,26 +8,153 @@
 SOCKET connections[100]; //creating and array to store all connections
 int connectionCounter = 0; //use as index of the array
 
-void clientHandlerThread(int index) {
+std::string serverIP = "145.85.48.4";
+int serverPort = 1337;
 
+enum Packet {
+	P_ChatMessage,
+	P_Test
+};
+
+bool sendInt(int index, int num) {
+	int retCheck = send(connections[index], (char*)&num, sizeof(int), NULL); //send the int
+
+	if (retCheck == SOCKET_ERROR) {
+		//connection problem
+		return false;
+	}
+
+	return true;
+
+}
+
+
+bool getInt(int index,int &num) {
+	int retCheck = recv(connections[index], (char*)&num, sizeof(int), NULL); //receive buffer lenght
+																	 //send the int
+
+	if (retCheck == SOCKET_ERROR) {
+		//connection problem
+		return false;
+	}
+
+	return true;//Sucessfully received int
+
+}
+
+bool sendPacketType(int index,Packet packettype) {
+	int retCheck = send(connections[index], (char*)&packettype, sizeof(Packet), NULL); //Sending the packet type
+
+	if (retCheck == SOCKET_ERROR) {
+		//connection problem
+		return false;
+	}
+
+	return true;
+
+}
+
+bool getPacketType(int index,Packet &packettype) {
+	int retCheck = recv(connections[index], (char*)&packettype, sizeof(Packet), NULL);
+
+	if (retCheck == SOCKET_ERROR) {
+		//connection problem
+		return false;
+	}
+
+	return true;//Sucessfully received int
+
+}
+
+bool sendString(int index,std::string &text) {
+	if (!sendPacketType(index,P_ChatMessage)) {//sending the packat type
+		return false;
+	}
+
+	int bufferlength = text.size();
+	if (!sendInt(index,bufferlength)) {//sending the length of the message
+		return false;
+	}
+
+	int retCheck = send(connections[index], text.c_str(), bufferlength, NULL); //Sending the packet type
+
+	if (retCheck == SOCKET_ERROR) {
+		//connection problem
+		return false;
+	}
+
+	return true;
+
+}
+
+bool getString(int index,std::string &text) {
 	int bufferLength;
-	while (true) {
-		recv(connections[index], (char*)&bufferLength, sizeof(int), NULL); // Receiving the length o the message
-		char * buffer = new char[bufferLength];//Allocating memory to a dynamic sized char array.
-		recv(connections[index], buffer, bufferLength, NULL);//Receiving the actual message
-		
-		
-		
-		for (int i = 0; i < connectionCounter; i++) {
-			if (i == index) {
-				continue;//Skip this index
+
+	if (!getInt(index,bufferLength)) {
+		return false;
+	}
+
+	char * buffer = new char[bufferLength + 1];//Allocating memory to a dynamic sized char array.
+	buffer[bufferLength] = '\0';//Adding the null terminator to the end of the string
+
+	int retCheck = recv(connections[index], buffer, bufferLength, NULL);
+	text = buffer;//set the string to received buffer message
+	delete[] buffer; //everytime we use a "new" we should put a "delete" after we use it
+
+	if (retCheck == SOCKET_ERROR) {
+		//connection problem
+		return false;
+	}
+
+	return true;//Sucessfully received int
+
+}
+
+
+bool processPacket(int index,Packet packettype) {
+	switch (packettype) {
+		case P_ChatMessage:
+		{
+			std::string message;
+
+			if (!getString(index, message)) {
+				return false;
 			}
-			send(connections[i], (char*)&bufferLength, sizeof(int), NULL);
-			send(connections[i], buffer, bufferLength, NULL);
+
+			for (int i = 0; i < connectionCounter; i++) {
+				if (i == index) {
+					continue;//skip to next user because this is the sender
+				}
+
+				if (!sendString(i, message)) {
+					//send message to connection at index 1
+					std::cout << "Failed to send message from client " << index + 1 << " to client :" << i << std::endl;
+				}
+				std::cout << "Processed chat message packet from user : " << index + 1 << std::endl;
+				break;
+			}
 		}
 
-		delete[] buffer;//clearing out memory we reserved 
+		default:
+			std::cout << "Unrecognized packet:" << packettype << std::endl;
+			break;
+		}
+	return true;
+}
+void clientHandlerThread(int index) {
+	Packet packettype;
+	while (true) {
+
+		if (!getPacketType(index,packettype)) {
+			break;
+		}
+		if (!processPacket(index,packettype)) {
+			//if we couldn't process the packet then get out of the loop
+			break;
+		}
 	}
+	std::cout << "Lost connection to client "<< index+1 << std::endl;
+	closesocket(connections[index]); //Closing the socket once we are done with the connection
 }
 
 
@@ -43,8 +170,8 @@ int main() {
 
 	SOCKADDR_IN addr; //Address that we will bind our listeing socket to
 	int addrlen = sizeof(addr); //length of the address
-	addr.sin_addr.s_addr = inet_addr("127.0.0.1");//which address will the server be hosted on
-	addr.sin_port = htons(1111);//port. WHY NO FUNCTION FOR BOTH?
+	addr.sin_addr.s_addr = inet_addr(serverIP.c_str());//which address will the server be hosted on
+	addr.sin_port = htons(serverPort);//port. WHY NO FUNCTION FOR BOTH?
 	addr.sin_family = AF_INET;//Specifies an IPv4 type of socket
 	
 	//Create a socket that will listen to new connections
@@ -52,7 +179,7 @@ int main() {
 	SOCKET sListen = socket(AF_INET, SOCK_STREAM, NULL);//Socket to listen for new connections
 	bind(sListen, (SOCKADDR*)&addr, sizeof(addr));//Bind the address to the socket 
 	listen(sListen, SOMAXCONN);//Listening to socket with a maximum connection tries of SOMAXCONN->0x7ffffff
-	std::cout << "Server listening on 127.0.0.1:1111" << std::endl;
+	std::cout << "Server listening on " << serverIP.c_str() << std::endl;
 	//After we start listening, we need to declare a new connection
 	SOCKET newConnection;
 
@@ -64,14 +191,14 @@ int main() {
 			std::cout << "Failed to accept client's connection" << std::endl; //How to get the IP of the client?
 		}
 		else {
-			std::cout << "Client connected on port 1111" << std::endl; //put port and IP on variables up top for easily changing them
-			std::string MOTD = "Welcome! This is the Message Of The Day.";
-			int MOTDLen = MOTD.size();
-			send(newConnection, (char*)&MOTDLen, sizeof(int), NULL);
-			send(newConnection, MOTD.c_str(), sizeof(MOTD), NULL);//Send message to a new connection. A pointer to the new message, size of it, and flags
+			std::cout << "Client connected on port "<< serverPort << std::endl; //put port and IP on variables up top for easily changing them
 			connections[i] = newConnection;
 			connectionCounter++;
 			CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)clientHandlerThread, (LPVOID)(i), NULL, NULL);
+
+			std::string MOTD = "Welcome to the coolest server in the UNIverse!";
+			sendString(i, MOTD);
+	
 		}
 	}
 	
